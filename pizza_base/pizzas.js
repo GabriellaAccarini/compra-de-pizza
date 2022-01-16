ed31ea962246e3093c868b53d36edd2bd11b3572
Gabriella Accarini <gabriellaaccarini@hotmail.com>
2022-01-16 15:33:26 -0300

    projeto utilizando javascrit - Sistema de compra de pizzas

---
+++ pizza_base/pizzas.js
@@ -0,0 +1,9 @@
+let pizzaJson = [
+    {id:1, name:'Mussarela', img:'images/pizza.png', price:20.19, sizes:['100g', '530g', '860g'], description:'Queijo mussarela, molho de tomate, tomates em rodelas, orégano e azeitonas'},
+    {id:2, name:'Calabresa', img:'images/pizza2.png', price:18.00, sizes:['320g', '530g', '860g'], description:'Linguiça calabresa defumada, cebola, azeitonas pretas, orégano, queijo parmesão'},
+    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:17.45, sizes:['320g', '530g', '860g'], description:'Molho de tomate, queijo mussarela, gorgonzola, parmesão, provolene, orégano'},
+    {id:4, name:'Americana', img:'images/pizza4.png', price:19.77, sizes:['320g', '530g', '860g'], description:'Molho de tomate, queijo mussarela, gorgonzola, pepperoni, orégano, azeitonas'},
+    {id:5, name:'Marguerita', img:'images/pizza5.png', price:21.43, sizes:['320g', '530g', '860g'], description:'Molho de tomate, mussarela, tomates em rodelas, mangericão a gosto, azeite'},
+    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:18.55, sizes:['320g', '530g', '860g'], description:'Molho de tomate, mussarela, queijo prato, presunto, cebola, ovos cozidos'},
+    {id:7, name:'Chocolate', img:'images/pizza7.png', price:22.36, sizes:['320g', '530g', '860g'], description:'Chocolate ao leite, chocolate meio amargo, chocolate granulado, morangos'}
+];
